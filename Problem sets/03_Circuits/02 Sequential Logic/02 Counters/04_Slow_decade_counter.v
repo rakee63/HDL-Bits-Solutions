af6843eaaf610f9b730af377f8f31572334220b2
Rakesh Andavarapu <andavarapurakesh@gmail.com>
2025-08-10 15:19:09 +0530

    Create 04_Slow_decade_counter.v

---
+++ Problem sets/03_Circuits/02 Sequential Logic/02 Counters/04_Slow_decade_counter.v
@@ -0,0 +1,13 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+    always @(posedge clk) begin
+        if(reset) q <= 0;
+        else begin
+            if(slowena) q <= (q == 4'd9) ? 0 : q+4'd1;
+            else q <= q;
+        end
+    end
+endmodule
