e11e2bb26df1dbb0de4c71599a5bc9d5d98b0e36
Rakesh Andavarapu <andavarapurakesh@gmail.com>
2025-08-10 15:26:12 +0530

    Create 07_Shift_register.v

---
+++ Problem sets/03_Circuits/02 Sequential Logic/03 Shift registers/07_Shift_register.v
@@ -0,0 +1,21 @@
+module top_module (
+    input clk,
+    input resetn,   // synchronous reset
+    input in,
+    output out);
+	wire q1, q2, q3;
+    always @(posedge clk) begin
+        if(resetn==1'b0) begin
+            q1 <= 1'b0;
+            q2 <= 1'b0;
+            q3 <= 1'b0;
+            out <= 1'b0;
+        end
+        else begin
+            q1 <= in;
+            q2 <= q1;
+            q3 <= q2;
+            out <= q3;
+        end
+    end
+endmodule
