7d3f42d81bbc2a111c1cb6e03be16eed7708d837
Rakesh Andavarapu <andavarapurakesh@gmail.com>
2025-08-10 15:15:21 +0530

    Create 15_Detect_and_edge.v

---
+++ Problem sets/03_Circuits/02 Sequential Logic/01 Latches and Flip-flops/15_Detect_and_edge.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] pedge
+);
+    reg [7:0] in_prev;
+    always @(posedge clk) begin
+        pedge <= (~in_prev) & in;
+        in_prev <= in;
+    end
+
+endmodule
