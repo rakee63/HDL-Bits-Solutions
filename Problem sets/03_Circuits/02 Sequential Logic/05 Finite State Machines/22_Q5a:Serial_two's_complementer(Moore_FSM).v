41cfad4b3d4475dfbfd7a0f916a6f35a09538f73
Rakesh Andavarapu <andavarapurakesh@gmail.com>
2025-08-10 15:42:29 +0530

    Create 22_Q5a:Serial_two's_complementer(Moore_FSM).v

---
+++ Problem sets/03_Circuits/02 Sequential Logic/05 Finite State Machines/22_Q5a:Serial_two's_complementer(Moore_FSM).v
@@ -0,0 +1,29 @@
+module top_module (
+    input clk,
+    input areset,
+    input x,
+    output z
+); 
+	parameter 	A = 2'b00,
+    			D = 2'b01,
+    			B = 2'b10,
+    			C = 2'b11;
+    reg [1:0] state, next_state;
+    
+    always @(posedge clk, posedge areset) begin
+        if(areset) state <= A;
+        else state <= next_state;
+    end
+    
+    always @(*) begin
+        case(state)
+            A: next_state = (x) ? B : A;
+            B: next_state = (x) ? D : C;
+            C: next_state = (x) ? D : C;
+            D: next_state = (x) ? D : C;
+            default: next_state = A;
+        endcase
+    end
+    
+    assign z = state[1];
+endmodule
