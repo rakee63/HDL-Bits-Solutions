4053b95a33a5901967b0d51ca31b05a247c2b23b
Rakesh Andavarapu <andavarapurakesh@gmail.com>
2025-08-10 14:59:57 +0530

    Create 05_Signed_addition_overflow.v

---
+++ Problem sets/03_Circuits/01_Combinational_Logic/03 Arithmetic Circuits/05_Signed_addition_overflow.v
@@ -0,0 +1,13 @@
+module top_module (
+    input [7:0] a,
+    input [7:0] b,
+    output [7:0] s,
+    output overflow
+); //
+ 
+   assign s =	{a + b};
+    /* assign overflow = (a[7]!=b[7]) ? 0 : 
+        (a[7]==s[7]) ? 0 : 1;*/
+    assign overflow = (a[7]==b[7]) ? ((a[7]==s[7]) ? 0 : 1):0;
+
+endmodule
