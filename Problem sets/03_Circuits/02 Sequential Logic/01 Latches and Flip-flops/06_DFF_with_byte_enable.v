9e001794b9c6f0df210e7a2304c414b5314ccdc1
Rakesh Andavarapu <andavarapurakesh@gmail.com>
2025-08-10 15:12:09 +0530

    Create 06_DFF_with_byte_enable.v

---
+++ Problem sets/03_Circuits/02 Sequential Logic/01 Latches and Flip-flops/06_DFF_with_byte_enable.v
@@ -0,0 +1,19 @@
+module top_module (
+    input clk,
+    input resetn,
+    input [1:0] byteena,
+    input [15:0] d,
+    output [15:0] q
+);
+    always @(posedge clk) begin
+        if (resetn) begin
+            if(byteena[0]) begin
+            	q[7:0] <= d[7:0];
+        	end
+        	if(byteena[1]) begin
+            	q[15:8] <= d[15:8];
+        	end
+        end
+        else q <= 0;
+    end
+endmodule
